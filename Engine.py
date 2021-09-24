70ef555a870bb625b8c964cd8e7e75f0b219cb3c
facileslopes <63903128+facileslopes@users.noreply.github.com>
2021-09-24 22:35:06 +0530

    Update Engine.py

--- Engine.py
+++ Engine.py
@@ -1,4 +1,3 @@
-import Ruleset
 game_state = []
 m_init_state = [
     ["N/A","bE", "-" , "bC", "-", "bD", "-", "bD", "-", "bC" ,"-" ,"bE","N/A"],
@@ -16,8 +15,7 @@ f_init_state = []#To be implemented
 game_log = []
 is_player_white = True
 #Variables to store how many times the white and black Pawn of Pawns have reached the end of the board
-b_pofp_ended = 0
-w_pofp_ended = 0
+pofp_ended = [0,0]
 class Move():
     def __init__(self, start_sq,end_sq, board):
         self.start_row = start_sq[0]
@@ -34,9 +32,9 @@ def make_move(board,move):
     new_board[move.end_row][move.end_col] = move.piece_moved
     return new_board
 
-def who_is_moving(board,move):
+def who_is_moving(board,location):
     #Returns True if a white piece is being moved, else returns False
-    piece = board[move.start_row][move.start_col]
+    piece = board[location[0]][location[1]]
     if piece[0] == "p":
         piece = piece[1]
     else:
@@ -47,14 +45,43 @@ def who_is_moving(board,move):
     if piece == "b":
         return False
 
-def promote_pieces(board):
+
+def promote_pieces(board,pofpended):
     #Promotes all pieces at the end of the turn
+    pofp_ended = pofpended
+    wpawn_moved = False
+    bpawn_moved = False
     for x in [0,9]:
         for y in range( len(board[x]) ):
             if board[x][y][0] == "p":
                 if board[x][y][2] == "K":
                     board[x][y] = board[x][y][1] + "P"
                 elif board[x][y][2] == "P":
-                    pass
+                    if board[x][y][1] == "w":
+                        wpawn_moved = True
+                        if pofp_ended[0] == 0:
+                            pofp_ended[0] += 1
+                        elif pofp_ended[0] == 2:
+                            board[x][y] = "-"
+                            board[7][6] = "pwP"
+                            pofp_ended[0] += 1
+                        elif pofp_ended[0] == 3:
+                            board[x][y] = "wP"
+                    else:
+                        bpawn_moved = True
+                        if pofp_ended[1] == 0:
+                            pofp_ended[1] += 1
+                        elif pofp_ended[1] == 2:
+                            board[x][y] = "-"
+                            board[2][6] = "pbP"
+                            pofp_ended[1] += 1
+                        elif pofp_ended[1] == 3:
+                            board[x][y] = "bP"
+                
                 else:
                     board[x][y] = board[x][y][1:]
+    if not(wpawn_moved) and pofp_ended[0] == 1:
+        pofp_ended[0] += 1
+    if not(bpawn_moved) and pofp_ended[1] == 1:
+        pofp_ended[1] += 1
+    return pofp_ended
