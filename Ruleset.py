0fca72745caf813d38a1b6e5253df8ee4762d842
facileslopes <63903128+facileslopes@users.noreply.github.com>
2021-10-06 11:34:21 +0530

    Add files via upload

--- Ruleset.py
+++ Ruleset.py
@@ -19,17 +19,46 @@ class Rules():
     def check_bounds(self,moves):
         #Makes sure the piece isn't moving anywhere it can't
         valid_moves = []
+        color = Rules.find_piece_color(self,location=self.location)
         for move in moves:
             if move[0] >= 0 and move[0] < rows and move[1] >= 0 and move[1] < columns:
                 #If the move is within the 10 rows and 13 columns
                 if self.gamestate[move[0]][move[1]] != "N/A":
                     #If the move isn't to an absent tile
                     valid_moves.append(move)
-        if self.gamestate[ self.location[0] ][ self.location[1] ][1] != "K":
+        for move in moves:
+            #Check if the piece is moving onto an immune pawn of pawn
+            if move[0] in [0,9]:
+                if self.gamestate[move[0]][move[1]] in ["pwP","pbP"]:
+                    valid_moves.remove(move)
+        if self.gamestate[ self.location[0] ][ self.location[1] ][1] not in ["K","A"]:
+            #Check if the correct piece is entering a citadel
             if (1,0) in valid_moves:
                 valid_moves.remove( (1,0) )
             elif (8,12) in valid_moves:
-                valid_moves.remove( (8,12) )     
+                valid_moves.remove( (8,12) )
+        else:
+            #Handles entering citadels for royal pieces
+            if color == "w":
+               if self.gamestate[ self.location[0] ][ self.location[1] ][1] == "A":
+                   if (1,0) in valid_moves:
+                       valid_moves.remove( (1,0) )
+               else: 
+                   if (8,12) in valid_moves:
+                       valid_moves.remove( (8,12) )
+                   if self.gamestate[1][0] == "bA":
+                       if (1,0) in valid_moves:
+                           valid_moves.remove( (1,0) )
+            else:
+               if self.gamestate[ self.location[0] ][ self.location[1] ][1] == "A":
+                   if (8,12) in valid_moves:
+                       valid_moves.remove( (8,12) )
+               else: 
+                   if (1,0) in valid_moves:
+                       valid_moves.remove( (1,0) )
+                   if self.gamestate[8][12] == "bA":
+                       if (8,12) in valid_moves:
+                           valid_moves.remove( (8,12) )
         return valid_moves
 
     def find_piece_color(self,location):
@@ -175,9 +204,13 @@ class Rules():
         return valid_moves
 
     def PrinceMoves(self):
-        #Prince moves like a king but can be captured
-        return Rules.return_valid_moves( self, [[0,1], [1,0] ,[-1,0], [0,-1],[1,1], [1,-1] ,[-1,1], [-1,-1]] )
-
+        #Prince moves like a king but cannot enter the opponent's citadel
+        valid_moves =  Rules.return_valid_moves( self, [[0,1], [1,0] ,[-1,0], [0,-1],[1,1], [1,-1] ,[-1,1], [-1,-1]] )
+        return valid_moves
+    def AdventiceMoves(self):
+        #An adventice king moves like a prince but can enter it's own citadel
+        valid_moves =  Rules.return_valid_moves( self, [[0,1], [1,0] ,[-1,0], [0,-1],[1,1], [1,-1] ,[-1,1], [-1,-1]] )
+        return valid_moves
     def PawnOfPawnsRules(self):
         #The pawn of pawns acts as a regular pawn until it moves to the final square, where it remains until a situation develops
         #where it guarantee a capture on any piece on the board, then it may be moved there.
@@ -268,12 +301,17 @@ class Rules():
     def KingMoves(self):
         #Moves like a King in chess
         valid_moves = Rules.return_valid_moves( self, [[0,1], [1,0] ,[-1,0], [0,-1],[1,1], [1,-1] ,[-1,1], [-1,-1]] )
-        valid_moves_copy = copy.deepcopy(valid_moves)
-        color = Rules.find_piece_color(self,self.location)
-        enemy_color = "b" if color == "w" else "w"
-        for move in valid_moves_copy:
-            if move in Engine.ZonesOfControls[enemy_color]:
-                valid_moves.remove(move)
+        if self.gamestate[self.location[0]][self.location[1]] in Engine.current_rulers:
+            color = Rules.find_piece_color(self,location=self.location)
+            colorint = 0 if color == "w" else 1
+            if Rules.is_ruler_check(self.gamestate,Engine.current_rulers[colorint]):
+                #Check if the piece can be swapped with one of it's own pieces
+                if not(Engine.king_swapped[colorint]):
+                    for x in range(rows):
+                        for y in range(columns):
+                            if self.gamestate[x][y] not in ["-","N/A","wK","bK","wA","wP"] and color == Rules.find_piece_color(self,[x,y]):
+                                if (x,y) not in valid_moves:
+                                    valid_moves.append((x,y))
         return valid_moves
     
     def check_if_pofp_canmove(board,player):
@@ -293,17 +331,16 @@ class Rules():
                     return True    
         return False              
                     
-    def is_king_threatened(self,move):
-        #Check if the player's king is being threatened currently
+    def is_ruler_threatened(self,move,ruler):
+        #Check if the player's ruler is being threatened currently
         piece = self.gamestate[self.location[0]][self.location[1]]
         gs = copy.deepcopy(self.gamestate)
         gs[self.location[0]][self.location[1]] = "-"
         gs[move[0]][move[1]] = piece
-        color = Rules.find_piece_color(self,location=self.location)
         for x in range(rows):
-            if color + "K" in gs[x]:
+            if ruler in gs[x]:
                 xloc = x
-                yloc = gs[x].index(color + "K")
+                yloc = gs[x].index(ruler)
         for x in range(rows):
             for y in range(columns):
                 if gs[x][y] not in ["-","N/A"]:
@@ -313,14 +350,71 @@ class Rules():
                         return True
                     
 
-    def check_king_vulnerable(self,moves):
-        #Checks which moves make the king vulnerable
+    def check_ruler_vulnerable(self,moves,ruler):
+        #Checks which moves make the ruler vulnerable
         moves_copy = copy.deepcopy(moves)
         for move in moves:
-            if Rules.is_king_threatened(self,move):
+            if Rules.is_ruler_threatened(self,move,ruler):
                 moves_copy.remove(move)
         return moves_copy
-                
+
+    def is_ruler_check(gs,ruler):
+        #Check if the ruler is under check
+        for x in range(rows):
+            if ruler in gs[x]:
+                xloc = x
+                yloc = gs[x].index(ruler)
+        for x in range(rows):
+            for y in range(columns):
+                if gs[x][y] not in ["-","N/A",Engine.current_rulers[0],Engine.current_rulers[1] ]:
+                    rule_check = Rules([x,y],gs)
+                    available_moves = rule_check.map_piece_to_rule()
+                    if (xloc,yloc) in available_moves:
+                        return True
+                    
+    def is_ruler_mated(gs,ruler):
+        #Check if a player has been checkmated or stalemated
+        for x in range(rows):
+            for y in range(columns):
+                if gs[x][y] not in ["-","N/A"]:
+                    rule_check = Rules([x,y],gs)
+                    moves = rule_check.map_piece_to_rule()
+                    moves = rule_check.check_ruler_vulnerable(moves,ruler)
+                    if len(moves) != 0:
+                        return False
+        return True
+    
+    def find_current_ruler(gs):
+        #Check if there are multiple royal pieces on the board of a certain color
+        ns = [0,1]
+        colors = ["w","b"]
+        royals = ["A","P","K"]
+        two_royals = [False,False]
+        for n in ns:
+            for x in range(rows):
+                for royal in royals:
+                    if colors[n] + royal in gs[x] and not(two_royals[n]):
+                        if Engine.current_rulers[n] == "-":
+                            Engine.current_rulers[n] = colors[n] + royal
+                        else:
+                            if colors[n] + royal != Engine.current_rulers[n]:
+                                Engine.current_rulers[n] = "-"
+                                two_royals[n] = True
+        
+    def citadel_check(gs):
+        #Checks whether a king has entered a citadel
+        if gs[1][0] == "wK":
+            if Engine.current_rulers[0] == "wK":
+                Engine.game_drawn = True
+            else:
+                Engine.game_drawn = True
+                Engine.swap_made[0] = True
+        elif gs[8][12] == "bK":
+            if Engine.current_rulers[1] == "bK":
+                Engine.game_drawn = True
+            else:
+                Engine.game_drawn = True
+                Engine.swap_made[1] = True
     def map_piece_to_rule(self):
         #Checks the piece to see what rule applies to it
         piece_identity = self.gamestate[ self.location[0] ][ self.location[1] ]
@@ -351,187 +445,9 @@ class Rules():
                 return Rules.KingMoves(self)
             elif piece_identity == "P":
                 return Rules.PrinceMoves(self)
+            elif piece_identity == "A":
+                return Rules.AdventiceMoves(self)
             else:
                 return Rules.MinisterMoves(self)
          
-    def return_zoc(self,transforms):
-        #Finds the zone of control(the squares which a piece is attacking)
-        return Rules.check_bounds(self, Rules.transform_piece(self, transforms) ) 
 
-    def PawnZOC(self):
-        #Finds the ZOC of a pawn
-        if Rules.find_piece_color(self,location=self.location) == "w":
-            return Rules.return_zoc(self,[[-1,-1],[-1,1]])
-        else:
-            return Rules.return_zoc(self,[[1,-1],[1,1]])
-
-    def MinisterZOC(self):
-        #Finds the ZOC of a minister
-        return Rules.return_zoc( self, [[1,1], [1,-1] ,[-1,1], [-1,-1]] )
-    
-    def VizierZOC(self):
-        #Finds the ZOC of a vizier
-        return Rules.return_zoc( self, [[1,0], [0,-1] ,[-1,0], [0,1]] )
-
-    def CamelZOC(self):
-        return Rules.return_zoc( self, [[3,1],[3,-1],[-3,-1],[-3,1],[1,3],[1,-3],[-1,3],[-1,-3]])
-
-    def ElephantZOC(self):
-        return Rules.return_zoc( self, [[2,2], [2,-2] ,[-2,2], [-2,-2]] )
-
-    def DabbabaZOC(self):
-        return Rules.return_zoc( self, [[0,2], [2,0] ,[-2,0], [0,-2]] )
-
-    def HorseZOC(self):
-        return Rules.return_zoc( self, [[2,1],[2,-1],[-2,-1],[-2,1],[1,2],[1,-2],[-1,2],[-1,-2]])
-
-    def KingZOC(self):
-        return Rules.return_zoc(self, [[1,0], [0,-1] ,[-1,0], [0,1],[1,1], [1,-1] ,[-1,1], [-1,-1]] )
-
-    def RookZOC(self):
-        n = 1
-        possible_moves = []
-        directions = [[0,1], [1,0] ,[-1,0], [0,-1]]
-        can_move_in_direction = []
-        color = Rules.find_piece_color(self,location=self.location)
-        enemy_king = "bK" if color == "w" else "wK"
-        for direction in directions:
-            can_move_in_direction.append(True)
-        while True in can_move_in_direction:
-            for direction in directions:
-                if can_move_in_direction[directions.index(direction)]:
-                    #First find the value of the transform
-                    transform = n * np.array(direction)    
-                    transform = transform.tolist()
-                    move = Rules.return_zoc(self,[transform])
-                    if len(move) != 0:
-                        possible_moves.append(move[0])
-                        move = list(move[0])
-                        if self.gamestate[move[0]][move[1]] != '-' and self.gamestate[move[0]][move[1]] != enemy_king:
-                            can_move_in_direction[directions.index(direction)] = False
-                    else:
-                        can_move_in_direction[directions.index(direction)] = False
-            n += 1
-        return possible_moves
-
-    def ScoutZOC(self):
-        n = 1
-        possible_moves = []
-        directions = [[1,1], [1,-1] ,[-1,1], [-1 ,-1]]
-        can_move_in_direction = []
-        color = Rules.find_piece_color(self,location=self.location)
-        enemy_king = "bK" if color == "w" else "wK"
-        for direction in directions:
-            can_move_in_direction.append(True)
-        while True in can_move_in_direction:
-            for direction in directions:
-                if can_move_in_direction[directions.index(direction)]:
-                    #First find the value of the transform
-                    transform = n * np.array(direction)    
-                    transform = transform.tolist()
-                    move = Rules.return_zoc(self,[transform])
-                    if len(move) != 0:
-                        possible_moves.append(move[0])
-                        move = list(move[0])
-                        if self.gamestate[move[0]][move[1]] != '-' and self.gamestate[move[0]][move[1]] != enemy_king:
-                            can_move_in_direction[directions.index(direction)] = False
-                    else:
-                        can_move_in_direction[directions.index(direction)] = False
-            n += 1
-        exclude_moves = tuple(Rules.transform_piece( self, [[1,1], [1,-1] ,[-1,1], [-1,-1]] ))
-        if len(possible_moves) != 0:
-            for move in exclude_moves:
-                if move in possible_moves:
-                    possible_moves.remove(move)
-        return possible_moves
-
-    def GiraffeZOC(self):
-        diag_transforms = [[1,1], [1,-1] ,[-1,1], [-1,-1]]
-        can_move_in_direction = []
-        diag_moves = Rules.return_valid_moves( self, diag_transforms )
-        diag_transforms = []
-        n = 1
-        valid_moves = []
-        can_move = False
-        color = Rules.find_piece_color(self,location=self.location)
-        enemy_king = "bK" if color == "w" else "wK"
-        #First check where it can move from after moving one step diagonally
-        for move in diag_moves:
-            can_move_in_direction.append([True,True])
-            diag_transforms.append( [move[0] - self.location[0], move[1] - self.location[1] ] )
-        for direction in can_move_in_direction:
-            for x in direction:
-                if x:
-                    can_move = True
-        while can_move:
-            for move in diag_moves:
-                for x in range(0,2):
-                    if x == 0:
-                        curr_move = [move[0] + diag_transforms[diag_moves.index(move)][x]*n, move[1] ]
-                    else:
-                        curr_move = [move[0], move[1]  + diag_transforms[diag_moves.index(move)][x]*n ]
-                    if can_move_in_direction[diag_moves.index(move)][x] == True:
-                        curr_move = Rules.check_bounds(self, [curr_move])
-                        if len(curr_move) != 0:
-                            if n >= 3:
-                                valid_moves.append(tuple(curr_move[0]))
-                            lmove = list(curr_move[0])
-                            if self.gamestate[lmove[0]][lmove[1]] != '-' and self.gamestate[move[0]][move[1]] != enemy_king:
-                                can_move_in_direction[diag_moves.index(move)][x] = False
-                        else:
-                            can_move_in_direction[diag_moves.index(move)][x] = False
-            can_move = False
-            for direction in can_move_in_direction:
-                for x in direction:
-                    if x:
-                        can_move = True
-            n += 1
-        return valid_moves
-
-        
-    def map_piece_to_zone_of_control(self):
-        #Checks the piece to see what zone of control it has
-        piece_identity = self.gamestate[ self.location[0] ][ self.location[1] ]
-        if piece_identity[0] == "p":
-            return Rules.PawnZOC(self)
-        else:
-            piece_identity = piece_identity[1]
-            if piece_identity == "C":
-                return Rules.CamelZOC(self)
-            elif piece_identity == "D":
-                return Rules.DabbabaZOC(self)
-            elif piece_identity == "E":
-                return Rules.ElephantZOC(self)
-            elif piece_identity == "R":
-                return Rules.RookZOC(self)
-            elif piece_identity == "N":
-                return Rules.HorseZOC(self)
-            elif piece_identity == "S":
-                return Rules.ScoutZOC(self)
-            elif piece_identity == "G":
-                return Rules.GiraffeZOC(self)
-            elif piece_identity == "V":
-                return Rules.VizierZOC(self)
-            elif piece_identity == "K":
-                return Rules.KingZOC(self)
-            elif piece_identity == "P":
-                return Rules.KingZOC(self)
-            else:
-                return Rules.MinisterZOC(self)
-
-    def update_zocs(gs):
-        #Returns the squares each player threaten
-        zoneofcontrols = {"w":[],"b":[]}
-        for x in range(rows):
-            for y in range(columns):
-                color = ""
-                zoc = []
-                if gs[x][y] not in ["N/A","-"]:
-                    Piece = Rules([x,y],gs)
-                    zoc = Piece.map_piece_to_zone_of_control()
-                    color = Rules.find_piece_color(Piece,[x,y])
-                if len(zoc) != 0:
-                    for move in zoc:
-                        if move not in zoneofcontrols[color]:
-                            zoneofcontrols[color].append(move)
-        return zoneofcontrols
